0aef8d1b2ca33384d4380876898d76e2ca81bfbd
Filip <fkania08@gmail.com>
2022-05-04 21:50:54 +0200

    Create validates for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_and_belongs_to_many :petrol_stations, :join_table => :users_petrol_stations
     has_many :posts
+
+    validates :name, presence: true, uniqueness: true, length: { in: 3..50 }
+    validates :index, presence: true, length: { is: 6 }, uniqueness: true
 end
