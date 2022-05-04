54cd7d6ac719f922924de05e9a897243488c6126
Filip <fkania08@gmail.com>
2022-05-04 21:41:13 +0200

    Create static controller and index.html

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   resources :petrol_stations
   resources :users
+  
+  root to: 'static#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
