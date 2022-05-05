61b2c42bff13a01f16cb8ffe1a89971c534c92cf
Klaudia <klaudia.jackiewiczz@gmail.com>
2022-05-05 20:17:46 +0200

    dodanie pliku 20220505200930_add_token_to_users.rb

--- db/migrate/20220505200930_add_token_to_users.rb
+++ db/migrate/20220505200930_add_token_to_users.rb
@@ -1,4 +1,4 @@
-class AddTokenToStudents < ActiveRecord::Migration[5.1]
+class AddTokenToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :token, :string
     add_index :users, :token
