781d75bdf8e758f9513bac60a054d3bfe3a44806
Filip <fkania08@gmail.com>
2022-05-10 23:49:23 +0200

    Create database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+users = User.create(
+    [
+        {
+            index: '253047',
+            name: 'Filip',
+            password: 'testtest1'
+        },
+        {
+            index: '047253',
+            name: 'PiliF',
+            password: 'testtest2'
+        }
+    ]
+)
+
+petrol_stations = PetrolStation.create(
+    [
+        {
+            name: 'Orlen (Wroclaw, Szybowcowa)',
+            address: 'ul. Szybowcowa 27, Wroclaw',
+        },
+        {
+            name: 'Shell (Wroclaw, Malopanewska)',
+            address: 'ul. Malopanewska 54, Wroclaw',
+        }
+    ]
+)
+
+# post_dates = PostDate.create(
+#     [
+#         {
+#             title: '6-05-2022',
+#             user: users.first,
+#             petrol_station: petrol_stations.first
+#         },
+#         {
+#             title: '4-05-2022',
+#             user: users.first,
+#             petrol_station: petrol_stations.last
+#         }
+#     ]
+# )
+
+#users.first.petrol_stations = petrol_stations
